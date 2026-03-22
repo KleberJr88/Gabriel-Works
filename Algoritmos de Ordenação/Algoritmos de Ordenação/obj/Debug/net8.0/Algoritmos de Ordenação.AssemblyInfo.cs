d6aa14baa67dfb0278336aedab97801dc09f84b9
KleberJr88 <Degemiossaga@gmail.com>
2026-03-22 10:45:23 -0300

    Tentativa de Abrir o Git

--- Algoritmos de Ordenação/Algoritmos de Ordenação/obj/Debug/net8.0/Algoritmos de Ordenação.AssemblyInfo.cs
+++ Algoritmos de Ordenação/Algoritmos de Ordenação/obj/Debug/net8.0/Algoritmos de Ordenação.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algoritmos de Ordenação")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff37418fa7eea933f6a937097bb5bd7c6d1e9bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9616ed617ef094efb79d4c1cae262c0f3522ceda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algoritmos de Ordenação")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algoritmos de Ordenação")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
